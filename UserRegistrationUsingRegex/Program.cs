ee773ceadba0fb2e076daa068f47c0b768c54f95
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-15 23:47:25 +0530

    [sohail] Add: Email validation

--- UserRegistrationUsingRegex/Program.cs
+++ UserRegistrationUsingRegex/Program.cs
@@ -9,16 +9,19 @@ namespace UserRegistrationUsingRegex
     /// <param name="args"></param>
         static void Main(string[] args)
         {
+            RegexValidation regex = new RegexValidation();
             Console.WriteLine("User registration Problem statement");
             Console.WriteLine("*First letter should be capital.");
             Console.WriteLine("*length should be Minimum three characters.");
             Console.WriteLine("Enter First name :");
             string fname = Console.ReadLine();
-            RegexValidation regex = new RegexValidation();
             regex.CheckName(fname);
             Console.WriteLine("Enter Last name :");
             string lname = Console.ReadLine();
             regex.CheckName(lname);
+            Console.WriteLine("Enter Email id :");
+            string email = Console.ReadLine();
+            regex.CheckEmail(email);
         }
     }
 }
--- UserRegistrationUsingRegex/RegexValidation.cs
+++ UserRegistrationUsingRegex/RegexValidation.cs
@@ -13,11 +13,26 @@ namespace UserRegistrationUsingRegex
             try
             {
                 if(Regex.IsMatch(name, validateFirstName))
-                    Console.WriteLine(" Name saved succesfull");
+                    Console.WriteLine(" Name validate succesfull");
                 else
                     Console.WriteLine("Invalid name!!");
             }
             catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CheckEmail(string email)
+        {
+            string validateEmail = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            try
+            {
+                if (Regex.IsMatch(email, validateEmail))
+                    Console.WriteLine(" email validate succesfull");
+                else
+                    Console.WriteLine("Invalid email!!");
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
