5cb8597044e6c3abfe6b7d02757ef02af369a36b
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-16 01:32:34 +0530

    [sohail] Add: Rule-2: Atleast 1 Upper case character

--- UserRegistrationUsingRegex/RegexValidation.cs
+++ UserRegistrationUsingRegex/RegexValidation.cs
@@ -55,7 +55,7 @@ namespace UserRegistrationUsingRegex
 
         internal void CheckPassword(string password)
         {
-            string validatepassword = "^[A-Za-z0-9]{8,20}$";
+            string validatepassword = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
             try
             {
                 if (Regex.IsMatch(password, validatepassword))
