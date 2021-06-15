8511a08319fb0afb273dd1469f864e2361e2b821
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-15 23:18:00 +0530

    [sohail] Add: First name validation

---
+++ UserRegistrationUsingRegex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UserRegistrationUsingRegex
+{
+    class Program
+    {   /// <summary>
+    /// UC1-First Name validation
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User registration Problem statement");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            Console.WriteLine("Enter First name :");
+            string fname = Console.ReadLine();
+            RegexValidation regex = new RegexValidation();
+            regex.CheckFirstName(fname);
+        }
+    }
+}
---
+++ UserRegistrationUsingRegex/RegexValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingRegex
+{
+    class RegexValidation
+    {
+        public void CheckFirstName(string firstname)
+        {
+            string validateFirstName = "^[A-Z][a-z]{3,20}$";
+            try
+            {
+                if(Regex.IsMatch(firstname, validateFirstName))
+                    Console.WriteLine("First Name saved succesfull");
+                else
+                    Console.WriteLine("Invalid name!!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
